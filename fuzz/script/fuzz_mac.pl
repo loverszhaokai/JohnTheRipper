1940c15f48058d8214c9470d27f0f1a64a9ed560
Kai Zhao <loverszhao@gmail.com>
2015-06-11 21:11:06 +0800

    Update fuzz_mac.pl: Add mutate method: 1. change case 2. insert strings from dictionary

--- fuzz/script/fuzz_mac.pl
+++ fuzz/script/fuzz_mac.pl
@@ -27,15 +27,264 @@ die unless (mkdir($workdir, 0700) || $!{EEXIST});
 $ENV{'OMP_NUM_THREADS'} = '1';
 setpriority(PRIO_PROCESS, 0, 19);
 
-if (1 != $#ARGV) {
-    print "usage: ./fuzz.pl /path/to/john format-name\n";
+if (1 > $#ARGV || 2 < $#ARGV) {
+    print "usage: ./fuzz.pl /path/to/john format-name [dictionary]\n";
     die;
 }
 
 $john_path = $ARGV[0];
 $format_name = $ARGV[1];
+$dictionary_file = "";
 
-sub try
+if (2 == $#ARGV) {
+	$dictionary_file = $ARGV[2];
+}
+
+#
+# Init dictionary file
+#
+open($dic_handler, "<", $dictionary_file)
+	|| die "dictionary_file=$dictionary_file does not exist\n";
+while ($line = <$dic_handler>) {
+	chomp $line;
+	$dictionary[$#dictionary + 1] = $line;
+}
+close($dic_handler);
+
+#
+# Get all formats name
+#
+open(FORMATS, "$john_path --list=formats --format=$format_name |") || die;
+while (<FORMATS>) {
+	($line) = /^([ ,\w\d-]+)/;
+	@words = split(/, /, $line);
+	for ($i = 0; $i <= $#words; $i++) {
+		$formats[$#formats + 1] = $words[$i];
+	}
+}
+close(FORMATS);
+
+#
+# Get all test cases in each format
+#
+for ($i = 0; $i <= $#formats; $i++) {
+	print "i=$i  ";
+	print $formats[$i];
+	print "\n";
+
+	open(TESTS, "$john_path --list=format-tests --format=$formats[$i] |") || die;
+	while (<TESTS>) {
+		($f, $c) = /^([\w\d-]+)\t[^\t]+\t([^\t]+)\t/;
+		if ($f && $c) {
+			$fs[$#fs + 1] = $f;
+			$cs[$#cs + 1] = $c;
+		}
+	}
+	close(TESTS);
+}
+
+die unless ($#fs >= 0 && $#fs == $#cs);
+
+printf "%u test vectors\n", $#fs + 1;
+
+$cpus = `sysctl -n hw.ncpu`;
+chomp $cpus;
+$cpus = 1 if (!$cpus);
+
+print "$cpus CPUs\n";
+
+$cpus *= $factor;
+
+$cpu = 1;
+
+for ($cpu = 0; $cpu < $cpus - 1; $cpu++) {
+	last if (fork() == 0);
+}
+
+$from = int(($#fs + 1) * $cpu / $cpus);
+$to = int(($#fs + 1) * ($cpu + 1) / $cpus) - 1;
+
+print "from=$from\n";
+print "to=$to\n";
+
+$session .= "-$cpu";
+$pot .= "-$cpu";
+$pwfile .= "-$cpu";
+
+$seq = 0;
+
+for ($t = $from; $t <= $to; $t++) {
+	$f = $fs[$t];
+	$c = $cs[$t];
+	$o = $c;
+
+	print "\n";
+	print "format        = $f\n";
+	print "original hash = $o\n";
+
+	Run();
+
+        print "\n";
+        print "->9\n\n";
+	# Replace chars with '9'
+	for ($i = 0; $i < length($c); $i++) {
+		vec($c, $i, 8) = ord('9');
+		next if ($c eq $o);
+		#print "new=$c\n";
+		Run();
+		$c = $o;
+	}
+
+	print "\n";
+	print "->\$\n\n";
+	# Replace chars with '$'
+	for ($i = 0; $i < length($c); $i++) {
+		vec($c, $i, 8) = ord('$');
+		next if ($c eq $o);
+		#print "new=$c\n";
+		Run();
+		$c = $o;
+	}
+
+	print "\n";
+	print "->*\n\n";
+	# Replace chars with '*'
+	for ($i = 0; $i < length($c); $i++) {
+		vec($c, $i, 8) = ord('*');
+		next if ($c eq $o);
+		#print "new=$c\n";
+		Run();
+		$c = $o;
+	}
+
+	print "\n";
+	print "->#\n\n";
+	# Replace chars with '#'
+	for ($i = 0; $i < length($c); $i++) {
+		vec($c, $i, 8) = ord('#');
+		next if ($c eq $o);
+		#print "new=$c\n";
+		Run();
+		$c = $o;
+	}
+
+	print "\n";
+	print "swap two adjacent chars\n\n";
+	# Swap two adjacent chars
+	for ($i = 0; $i < length($c) - 1; $i++) {
+		my $x = vec($c, $i, 8);
+		vec($c, $i, 8) = vec($c, $i + 1, 8);
+		vec($c, $i + 1, 8) = $x;
+		next if ($c eq $o);
+		#print "new=$c\n";
+		Run();
+		$c = $o;
+	}
+
+	print "\n";
+	print "Append many copies of the last char\n\n";
+	# Append many copies of the last char
+	$j = 1;
+	for ($i = 0; $i < 5; $i++) {
+		print "j=$j\n";
+		$c = $o . chr(vec($o, length($o) - 1, 8)) x $j;
+		#print "new=$c\n";
+		Run();
+		$j *= ($j + 1);
+	}
+
+        ChangeCase();
+
+        InsertDictionary();
+}
+
+#
+# 1. Change each char to upper case and lower case
+# 2. Change all chars to upper case and lower case
+#
+sub ChangeCase
+{
+	print "\nChangecase\n\n";
+
+	$c =$o;
+
+	# Change chars to Upper Case
+	for ($i = 0; $i < length($c); $i++) {
+		$char = chr(vec($c, $i, 8));
+		if ($char =~ /[a-z]/ || $char =~ /[A-Z]/) {
+			$char = uc $char;
+			vec($c, $i, 8) = vec($char, 0, 8);
+			next if ($c eq $o);
+			#print "new=$c\n";
+			Run();
+		}
+		$c = $o;
+	}
+
+	print "\n";
+	# Change chars to lower Case
+	for ($i = 0; $i < length($c); $i++) {
+		$char = chr(vec($c, $i, 8));
+		if ($char =~ /[a-z]/ || $char =~ /[A-Z]/) {
+			$char = lc $char;
+			vec($c, $i, 8) = vec($char, 0, 8);
+			next if ($c eq $o);
+			#print "new=$c\n";
+			Run();
+		}
+		$c = $o;
+	}
+
+	print "\n";
+	# Change all chars to upper Case
+	for ($i = 0; $i < length($c); $i++) {
+		$char = chr(vec($c, $i, 8));
+		if ($char =~ /[a-z]/ || $char =~ /[A-Z]/) {
+			$char = uc $char;
+			vec($c, $i, 8) = vec($char, 0, 8);
+		}
+	}
+	#print "new=$c\n";
+	Run();
+
+	print "\n";
+	# Change all chars to lower Case
+	for ($i = 0; $i < length($c); $i++) {
+		$char = chr(vec($c, $i, 8));
+		if ($char =~ /[a-z]/ || $char =~ /[A-Z]/) {
+			$char = lc $char;
+			vec($c, $i, 8) = vec($char, 0, 8);
+		}
+	}
+	#print "new=$c\n";
+	Run();
+}
+
+#
+# Insert strings from dictionary before each char
+#
+sub InsertDictionary
+{
+	print "\nInsertDictionary\n\n";
+
+	$c =$o;
+
+	# Change chars to Upper Case
+	for ($i = 0; $i <= length($c); $i++) {
+		print "insert before pos=$i\n";
+		for ($j = 0; $j <= $#dictionary; $j++) {
+			$c = substr($c, 0, $i) . $dictionary[$j] . substr($c, $i);
+			#print "new=$c\n";
+			Run();
+			$c = $o;
+		}
+	}
+}
+
+#
+# Run john
+#
+sub Run
 {
 	print "Trying format $f, hash $c\n";
 
@@ -60,83 +309,5 @@ sub try
 		print SAMPLE "$c\n";
 		close(SAMPLE);
 		$seq++;
-	}
-}
-
-open(TESTS, "$john_path --list=format-tests --format=$format_name |") || die;
-while (<TESTS>) {
-	($f, $c) = /^([\w\d-]+)\t[^\t]+\t([^\t]+)\t/;
-	if ($f && $c) {
-		$fs[$#fs + 1] = $f;
-		$cs[$#cs + 1] = $c;
-	}
-}
-close(TESTS);
-
-die unless ($#fs >= 0 && $#fs == $#cs);
-
-printf "%u test vectors\n", $#fs + 1;
-
-$cpus = `grep -c ^processor /proc/cpuinfo`;
-chomp $cpus;
-$cpus = 1 if (!$cpus);
-
-print "$cpus CPUs\n";
-
-$cpus *= $factor;
-
-for ($cpu = 0; $cpu < $cpus - 1; $cpu++) {
-	last if (fork() == 0);
-}
-
-$from = int(($#fs + 1) * $cpu / $cpus);
-$to = int(($#fs + 1) * ($cpu + 1) / $cpus) - 1;
-
-$session .= "-$cpu";
-$pot .= "-$cpu";
-$pwfile .= "-$cpu";
-
-$seq = 0;
-
-for ($t = $from; $t <= $to; $t++) {
-	$f = $fs[$t];
-	$c = $cs[$t];
-	$o = $c;
-
-	print "cccc=$c\n";
-	print "oooo=$o\n";
-	
-	#try;
-
-	for ($i = 0; $i < length($c); $i++) {
-		vec($c, $i, 8) = ord('9');
-		next if ($c eq $o);
-		#try;
-		$c = $o;
-	}
-
-	next;
-
-	for ($i = 0; $i < length($c); $i++) {
-		vec($c, $i, 8) = ord('$');
-		next if ($c eq $o);
-		#try;
-		$c = $o;
-	}
-
-	for ($i = 0; $i < length($c) - 1; $i++) {
-		my $x = vec($c, $i, 8);
-		vec($c, $i, 8) = vec($c, $i + 1, 8);
-		vec($c, $i + 1, 8) = $x;
-		next if ($c eq $o);
-		#try;
-		$c = $o;
-	}
-
-	$j = 1;
-	for ($i = 0; $i < 5; $i++) {
-		$c = $o . chr(vec($o, length($o) - 1, 8)) x $j;
-		#try;
-		$j *= ($j + 1);
 	}
 }
