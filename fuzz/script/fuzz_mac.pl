ce0c3be2568c4c49867983b267997b48f2f3f3c9
loverszhaokai <loverszhao@gmail.com>
2015-06-11 13:27:16 +0800

    add fuzz_mac.pl

---
+++ fuzz/script/fuzz_mac.pl
@@ -0,0 +1,142 @@
+#!/usr/bin/perl -w
+
+#
+# Author: Alexander
+# Email: solar@openwall.com
+# Date: 2015-06-05
+#
+
+#
+# How to run
+#
+# ulimit -v 2097152; time ./fuzz_mac.pl /path/to/john format-name &> fuzz.log
+#
+
+use Errno;
+
+# Processes per logical CPU
+$factor = 4;
+
+$workdir = 'run';
+$pwfile = $workdir . '/pw';
+$session = $workdir . '/s';
+$pot = $workdir . '/pot';
+
+die unless (mkdir($workdir, 0700) || $!{EEXIST});
+
+$ENV{'OMP_NUM_THREADS'} = '1';
+setpriority(PRIO_PROCESS, 0, 19);
+
+if (1 != $#ARGV) {
+    print "usage: ./fuzz.pl /path/to/john format-name\n";
+    die;
+}
+
+$john_path = $ARGV[0];
+$format_name = $ARGV[1];
+
+sub try
+{
+	print "Trying format $f, hash $c\n";
+
+	open(PW, "> $pwfile") || die;
+	print PW "$c\n";
+	close(PW);
+
+	open(JOHN, "| $john_path --skip-self-tests --nolog --encoding=raw --stdin --session=$session --pot=$pot --format=$f $pwfile") || die;
+	print JOHN "wrong password " x10 . "one\n";
+	print JOHN "two wrongs\n";
+	print JOHN "wrong password three\n";
+	close(JOHN);
+
+	die if ($? == 256 || $? == 2 || $? == 15); # exit(1) or INT or TERM
+
+	if ($? != 0) {
+		open(LOG, ">> fuzz-err.log") || die;
+		print LOG "$f $c $?\n";
+		close(LOG);
+
+		open(SAMPLE, "> fuzz-sample-$f-$cpu-$seq") || die;
+		print SAMPLE "$c\n";
+		close(SAMPLE);
+		$seq++;
+	}
+}
+
+open(TESTS, "$john_path --list=format-tests --format=$format_name |") || die;
+while (<TESTS>) {
+	($f, $c) = /^([\w\d-]+)\t[^\t]+\t([^\t]+)\t/;
+	if ($f && $c) {
+		$fs[$#fs + 1] = $f;
+		$cs[$#cs + 1] = $c;
+	}
+}
+close(TESTS);
+
+die unless ($#fs >= 0 && $#fs == $#cs);
+
+printf "%u test vectors\n", $#fs + 1;
+
+$cpus = `grep -c ^processor /proc/cpuinfo`;
+chomp $cpus;
+$cpus = 1 if (!$cpus);
+
+print "$cpus CPUs\n";
+
+$cpus *= $factor;
+
+for ($cpu = 0; $cpu < $cpus - 1; $cpu++) {
+	last if (fork() == 0);
+}
+
+$from = int(($#fs + 1) * $cpu / $cpus);
+$to = int(($#fs + 1) * ($cpu + 1) / $cpus) - 1;
+
+$session .= "-$cpu";
+$pot .= "-$cpu";
+$pwfile .= "-$cpu";
+
+$seq = 0;
+
+for ($t = $from; $t <= $to; $t++) {
+	$f = $fs[$t];
+	$c = $cs[$t];
+	$o = $c;
+
+	print "cccc=$c\n";
+	print "oooo=$o\n";
+	
+	#try;
+
+	for ($i = 0; $i < length($c); $i++) {
+		vec($c, $i, 8) = ord('9');
+		next if ($c eq $o);
+		#try;
+		$c = $o;
+	}
+
+	next;
+
+	for ($i = 0; $i < length($c); $i++) {
+		vec($c, $i, 8) = ord('$');
+		next if ($c eq $o);
+		#try;
+		$c = $o;
+	}
+
+	for ($i = 0; $i < length($c) - 1; $i++) {
+		my $x = vec($c, $i, 8);
+		vec($c, $i, 8) = vec($c, $i + 1, 8);
+		vec($c, $i + 1, 8) = $x;
+		next if ($c eq $o);
+		#try;
+		$c = $o;
+	}
+
+	$j = 1;
+	for ($i = 0; $i < 5; $i++) {
+		$c = $o . chr(vec($o, length($o) - 1, 8)) x $j;
+		#try;
+		$j *= ($j + 1);
+	}
+}
